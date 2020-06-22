34f2014432672db129ba8608a78ade552fc12d45
Adamo Fapohunda <adamofapohunda@gmail.com>
2020-06-22 12:06:32 +0200

    dummy sample with influx 2.0

--- src/influxdb-bridge.ts
+++ src/influxdb-bridge.ts
@@ -5,9 +5,58 @@
  */
 
 import { Adapter, } from 'gateway-addon';
-import { WebThingsClient } from 'webthings-client';
+import { WebThingsClient, Device } from 'webthings-client';
 import { client as WebSocketClient } from 'websocket';
-import { InfluxDB } from 'influx';
+import {InfluxDB, Point} from '@influxdata/influxdb-client';
+import { Option, None} from 'space-lift'
+
+
+enum ThingDataType {
+    BOOLEAN = "boolean",
+    NUMBER = "number",
+    INTEGER = "integer",
+    STRING = "string",
+}
+
+class PointFactory {
+
+    private name: string;
+    private deviceId: string;
+    
+    constructor (name:string, deviceId:string){
+        this.deviceId = deviceId;
+        this.name = name;
+    }
+
+    public getInfluxPoint(type: ThingDataType, val: any): Option<Point>{
+        switch(type) { 
+            case ThingDataType.NUMBER: { 
+                return Option(new Point(this.name)
+                    .tag('device', this.deviceId)
+                    .floatField('value', <number>val));               
+            } 
+            case ThingDataType.INTEGER: { 
+                return Option(new Point(this.name)
+                    .tag('device', this.deviceId)
+                    .intField('value', <number>val));   
+            } 
+            case ThingDataType.BOOLEAN: { 
+                return Option(new Point(this.name)
+                    .tag('device', this.deviceId)
+                    .booleanField('value', <boolean>val));   
+                } 
+            case ThingDataType.STRING: { 
+                return Option(new Point(this.name)
+                    .tag('device', this.deviceId)
+                    .stringField('value', <string>val));   
+            } 
+            default: { 
+                return None;  
+            } 
+         } 
+    }
+}
+
 
 interface ThingEvent {
     messageType: string,
@@ -25,34 +74,32 @@ export class InfluxDBBridge extends Adapter {
         const {
             host,
             port,
-            database
+            influxToken,
         } = this.manifest.moziot.config;
+
+        const token:string = influxToken
 
         console.log(`Connecting to influx at ${host}`);
 
-        const influxdb = new InfluxDB({
-            host,
-            port,
-            database
-        });
+        const url = `http://${host}:${port}`;
 
-        console.log('Create database if it does not exist');
+        const influxDb = new InfluxDB({url, token}) 
 
-        await influxdb.createDatabase(database);
-
-        this.connectToGateway(influxdb);
+        this.connectToGateway(influxDb);
     }
 
-    private connectToGateway(influxdb: InfluxDB) {
+    private connectToGateway(influxDb: InfluxDB) {
         console.log('Connecting to gateway');
 
         const {
-            accessToken
+            accessToken,
+            org,
+            bucket
         } = this.manifest.moziot.config;
 
         (async () => {
             const webThingsClient = await WebThingsClient.local(accessToken);
-            const devices = await webThingsClient.getDevices();
+            const devices: Device[] = await webThingsClient.getDevices();
 
             for (const device of devices) {
                 try {
@@ -84,9 +131,23 @@ export class InfluxDBBridge extends Adapter {
 
                                 if (thingEvent.messageType === 'propertyStatus') {
                                     if (Object.keys(thingEvent.data).length > 0) {
-                                        await influxdb.writeMeasurement(deviceId, [{
-                                            fields: thingEvent.data
-                                        }]);
+                                        Object.keys(thingEvent.data).forEach( key => {
+                                            
+                                            const point = new PointFactory(key, deviceId)
+                                                .getInfluxPoint(<ThingDataType>device.properties[key].type, (<any>thingEvent.data)[key])
+                 
+                                            point.forEach((p )=> {
+                                                const writeApi = influxDb
+                                                .getWriteApi(org, bucket);
+                                                // console.log(`TEST: device: ${deviceId} prop: ${key}, val ${p}`);
+                                                writeApi.writePoint(p);
+                                                writeApi
+                                                    .close()
+                                                    .catch((e: any) => {
+                                                        console.log('WRITE FAILED', e);
+                                                    });   
+                                            })
+                                        });
                                     }
                                 }
                             }
